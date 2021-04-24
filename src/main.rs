c8eff5ddbefd459967ac9b3d83ef9a747ff44730
Matej Sladek <matejsladek10@gmail.com>
2021-04-24 17:23:01 +0200

    append correct time

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{env, fs};
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Local};
 use std::path::PathBuf;
 use std::error::Error;
 
@@ -11,16 +11,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     let dir_path = PathBuf::from(dir_path);
     // println!("dir_path: {:?}", dir_path);
     let paths = fs::read_dir(dir_path.clone()).unwrap();
-    // return
     for path in paths {
         let path = path.unwrap().path();
         if path.is_dir() {
             continue
         }
-        // println!("Name: {}", .display());
-        let now: DateTime<Utc> = Utc::now();
-        let date_full = now.format("%Y_%m_%d_%H_%M_%S");
-        let date = now.format("%Y_%m_%d");
+        let metadata = fs::metadata(path.clone())?;
+        let created = metadata.created()?;
+        let created: DateTime<Local> = DateTime::from(created);
+        let date_full = created.format("_%Y_%m_%d_%H_%M_%S");
+        let date = created.format("%Y_%m_%d");
         let new_dir_path = dir_path.join(date.to_string());
         let _ = fs::create_dir(new_dir_path.clone());
         let ext = path.extension().unwrap().to_str().unwrap();
